69ab6fa4c08f9b74c0481ba7da70b7cc50a1ffcd
CmPunkLP <11redriver06@gmail.com>
2022-11-25 17:56:15 +0200

    1

---
+++ UI1/Assets/TestClick.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using UnityEngine;
+
+public class TestClick : MonoBehaviour {
+    public void ClickTest () {
+        Debug.Log ("clicked");
+   }
+
+   public void ClickTest2 (string text) {
+    Debug.Log (text);
+   }
+}
+
